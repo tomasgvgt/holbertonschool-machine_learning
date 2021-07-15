ed0cfea1c5024b07172fdc7b12f5122b6c9be8f4
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-15 18:25:48 -0500

    Size of a matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def matrix_shape(matrix):
+    "Calculates the shape of a matrix"
+    shape = []
+    for i in range(999999999999):
+        if isinstance(matrix, list):
+            shape.append(len(matrix))
+            matrix = matrix[0]
+        else:
+            return shape
+        i += i
