b667a1897ae7f539fbc3cce8599629e73c80adb3
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-15 18:45:34 -0500

    transpose a matrix

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 
 
 def matrix_transpose(matrix):
     """transposes a 2d matrix"""
     transpose = []
-    
     zipped_rows = zip(*matrix)
     transpose = [list(row) for row in zipped_rows]
     return transpose
