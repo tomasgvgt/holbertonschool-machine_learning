a628aca153d772e82dd4c3694bba4eb685c3441e
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-15 19:39:04 -0500

    11

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Transpose a np.ndarray"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Transposes a np.ndarray"""
-    return np.transpose(matrix)
+    return matrix.transpose()
