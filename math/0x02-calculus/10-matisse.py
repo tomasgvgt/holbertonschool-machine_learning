df8545c345be7f126bf849ac2e10926b33f77dd1
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-24 08:27:30 -0500

    Derivative function

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -3,6 +3,8 @@
 
 def poly_derivative(poly):
     derivative = []
+    if len(poly) == 0 or not isinstance(poly, list):
+        return None
     if len(poly) == 1:
         return [0]
     for i in range(1, len(poly)):
