10c6e0763cb306294681a836facd01e25f3f1333
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-24 08:38:06 -0500

    Integral

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,6 +4,12 @@
 def poly_integral(poly, C=0):
     integral = []
     num = 0
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    if any(not isinstance(i, (int, float)) for i in poly):
+        return None
+    if not isinstance(C, int):
+        return None
     for i in range(len(poly) + 1):
         if i == 0:
             integral.append(C)
