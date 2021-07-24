47d53d99d4b00b5921a1c7adad75fd5df4657b11
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-24 08:14:36 -0500

    Sumatory

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,8 @@
 def summation_i_squared(n):
     """Calculates a sum"""
     summation = 0
-    for i in range(1, n + 1):
+    if not isinstance(n, int):
+        return None
+    for i in range(n + 1):
         summation += i ** 2
     return summation
