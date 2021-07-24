39bbc58327ad77f2ab0f41afa8caa8637d14320f
tomasgvgt <tomasgvg.t@gmail.com>
2021-07-24 08:19:05 -0500

    Summation

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""Calculate a sum"""
 
 
 def summation_i_squared(n):
+    """Calculates a sum"""
     summation = 0
     if not isinstance(n, int):
         return None
-    for i in range(n + 1):
+    for i in range(n):
         summation += i ** 2
+    summation += n **2 
     return summation
