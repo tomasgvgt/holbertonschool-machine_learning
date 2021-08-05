73821c45c7bad1a8de166e38c99f116a081336dc
tomasgvgt <tomasgvg.t@gmail.com>
2021-08-05 10:40:38 -0500

    exponential

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -9,6 +9,7 @@ class Exponential:
         """
         Initializes the exponential distribution
         setting lambtha according to the given data
+
         """
         if data is None:
             if lambtha <= 0:
@@ -21,3 +22,26 @@ class Exponential:
             miu = sum(data) / len(data)
             lambtha = 1 / miu
         self.lambtha = float(lambtha)
+
+    def pdf(self, x):
+        """
+        Calculates the value of PDF for a given time period
+        """
+        # f (x; ℷ) = ℷ * e^(-ℷ * x)
+        if x < 0:
+            return 0
+        lamb = self.lambtha
+        e = 2.7182818285
+        return lamb * e ** (- lamb * x)
+
+    def cdf(self, x):
+        """
+        Calculates the value of CDF for a given time period
+        """
+        # F(x;  ℷ) = 1 - e **(-ℷ * x)
+        if x < 0:
+            return 0
+        lamb = self.lambtha
+        e = 2.7182818285
+        return 1 - e ** (-lamb * x)
+
