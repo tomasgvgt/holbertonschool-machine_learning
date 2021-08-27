baa64e2b4df1cf2d61190a172446fb12a5dca89a
tomasgvgt <tomasgvg.t@gmail.com>
2021-08-27 10:44:38 -0500

    -

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -7,10 +7,9 @@ def moving_average(data, beta):
     """calculates the weighted moving average of a data set"""
     moving_average = []
     val = 0
-    i = 0
-    bias_correction = 1 / ((1 - (beta ** (i + 1))))
 
     for i in range(len(data)):
-        val = (beta * val + (1 - beta) * data[i]) * bias_correction
-        moving_average.append(val)
+        val = (beta * val + (1 - beta) * data[i])
+        val_bias_corrected = val / (1 - (beta ** (i + 1)))
+        moving_average.append(val_bias_corrected)
     return moving_average
