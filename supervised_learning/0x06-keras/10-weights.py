404a5a54127d0ca4ecd9d2111671026e5746ccff
tomasgvgt <tomasgvg.t@gmail.com>
2021-09-20 09:11:46 -0500

    10

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Save and load the model weights
+"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    Saves the model weights
+    Arguments:
+        network: model whose weights should be saved
+        filename: path of the file that the weights should be saved to
+        save_format: format in which the weights should be saved
+    Return:
+        None
+    """
+
+    network.save_weights(filename)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    Loads the models weights
+    Arguments:
+        network: model to which the weights should be loaded
+        filename: path of the file that the weights should be loaded from
+    Return:
+        None
+    """
+    network.load_wheights(filename)
+    return None
