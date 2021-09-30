dba0c627c610cb307cb14e336e23707565731a06
tomasgvgt <tomasgvg.t@gmail.com>
2021-09-30 10:11:50 -0500

    -

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -31,19 +31,19 @@ def identity_block(A_prev, filters):
     kernel_init = K.initializers.he_normal(seed=None)
 
     lay1 = K.layers.Conv2D(kernel_size=(1, 1), filters=F11,
-                           padding='same', activation='relu',
+                           padding='same',
                            kernel_initializer=kernel_init)(A_prev)
     batch_norm1 = K.layers.BatchNormalization(axis=3)(lay1)
     activation1 = K.layers.Activation('relu')(batch_norm1)
 
     lay2 = K.layers.Conv2D(kernel_size=(3, 3), filters=F3,
-                           padding='same', activation='relu',
+                           padding='same',
                            kernel_initializer=kernel_init)(activation1)
     batch_norm2 = K.layers.BatchNormalization(axis=3)(lay2)
     activation2 = K.layers.Activation('relu')(batch_norm2)
 
     lay3 = K.layers.Conv2D(kernel_size=(1, 1), filters=F12,
-                           padding='same', activation='relu',
+                           padding='same',
                            kernel_initializer=kernel_init)(activation2)
     batch_norm3 = K.layers.BatchNormalization(axis=3)(lay3)
     add = K.layers.Add()([batch_norm3, A_prev])
