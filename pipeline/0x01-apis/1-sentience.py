20571531683cc4e6843503abe2cfc15538ee47a7
tomasgvgt <tomasgvg.t@gmail.com>
2022-04-11 11:58:21 -0500

    -

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -12,21 +12,16 @@ def sentientPlanets():
     of all sentient species.
     """
     species = requests.get("https://swapi-api.hbtn.io/api/species/").json()
-    sentient_species_urls = []
-    homeworld_urls = []
     planet_names = []
     while species['next'] is not None:
         for specie in species['results']:
-            if specie['designation'] == "sentient":
-                for people in specie['people']:
-                    sentient_species_urls.append(people)
-        species = requests.get(species['next']).json()
-    for people_url in sentient_species_urls:
-        subject = requests.get(people_url).json()
-        homeworld_urls.append(subject['homeworld'])
-    for planet_url in homeworld_urls:
-        planet = requests.get(planet_url).json()
-        planet_names.append(planet['name'])
+            designation = specie.get('designation', None)
+            classification = specie.get('classification', None)
+            if designation == "sentient" or classification == "sentient":
+                if specie['homeworld'] is not None:
+                    planet = requests.get(specie['homeworld']).json()
+                    planet_names.append(planet['name'])
+                else:
+                    continue
     planet_names = list(dict.fromkeys(planet_names))
-    print(len(planet_names))
     return planet_names
